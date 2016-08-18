31f8766c62e88aea528ddf0b67b4f9016816be0a
Rounak Salim <rounaksalim@gmail.com>
2016-08-17 21:28:15 -0500

    Cleaned up a little

--- server/app.js
+++ server/app.js
@@ -43,21 +43,8 @@ require('./routes')(app);
 var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({ port: 1024 });
 
-/*wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message, flags) {
-    if (flags.binary) {
-      console.log('Not managed yet...');
-    } else {
-      console.log('Received: %s', message);
-      ws.send('ping');
-    }
-  });
-});*/
-
 // Setup MongoOplog to check for changes in the database (parking data)
 var MongoOplog = require('mongo-oplog');
-var oplog = MongoOplog('mongodb://localhost:27017/local', { ns: 'garageinfo.info'}).tail();
-
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 var _ = require('lodash');
@@ -90,88 +77,27 @@ for (let i = 0; i < devices.length; ++i) {
       oplogHolder[j] = oplog;
       parkingDataHolder[j] = parkingData;
       collectionHolder[j] = collection;
-
-      /*var WebSocketServer = require('ws').Server,
-        wss = new WebSocketServer({ port: sensor.WebSocketPort });*/
-
-      // Setup the listener for any updates to the database
-     /* oplog.on('update', function (doc) {
-        console.log(JSON.stringify(doc));
-      });
-
-      oplog.on('insert', function (doc) {
-        database.getParkingDataWebSockets(parkingData.get(collection), function(err, result) {
-        console.log('INSERTED');
-        // Broadcast the message to every client 
-        wss.clients.forEach(function (client) {
-          client.send(JSON.stringify(result));
-          });
-        });
-      });
-
-      oplog.on('delete', function (doc) {
-        database.getParkingDataWebSockets(parkingData.get(collection), function(err, result) {
-          console.log('DELETED');
-          console.log(JSON.stringify(result));
-          // Broadcast the message to every client 
-          wss.clients.forEach(function (client) {
-            client.send(JSON.stringify(result));
-          });
-        });
-      });*/
     }
   }
-
-
- /* if (sensorIndex != -1) {
-      var sensor = device.sensors[sensorIndex];
-      var collection = sensor.collection;
-      var dbName = sensor.dbName;
-      var mongoUri = sensor.mongoAddress + '/' + dbName;
-      var parkingData = monk(mongoUri);
-      var oplog = MongoOplog('mongodb://' + mongoUri + '/local', { ns: dbName + '.' + collection}).tail();
-
-      var WebSocketServer = require('ws').Server,
-        wss = new WebSocketServer({ port: sensor.WebSocketPort });
-
-      // Setup the listener for any updates to the database
-      oplog.on('update', function (doc) {
-        console.log(JSON.stringify(doc));
-      });
-
-      oplog.on('insert', function (doc) {
-        database.getParkingDataWebSockets(parkingData.get(collection), function(err, result) {
-        // Broadcast the message to every client 
-        wss.clients.forEach(function (client) {
-          client.send(JSON.stringify(result));
-          });
-        });
-      });
-
-      oplog.on('delete', function (doc) {
-        database.getParkingDataWebSockets(parkingData.get(collection), function(err, result) {
-          // Broadcast the message to every client 
-          wss.clients.forEach(function (client) {
-            client.send(JSON.stringify(result));
-          });
-        });
-      });
-
-  }*/
 }
 
 for (let i = 0; i < oplogHolder.length; ++i) {
   // Setup the listener for any updates to the database
       oplogHolder[i].on('update', function (doc) {
-        console.log(JSON.stringify(doc));
+        database.getParkingDataWebSockets(collection, function(err, result) {
+          // Broadcast the message to every client 
+          wss.clients.forEach(function (client) {
+            client.send(JSON.stringify(result));
+          });
+        });
       });
 
       oplogHolder[i].on('insert', function (doc) {
         database.getParkingDataWebSockets(parkingDataHolder[i].get(collectionHolder[i]), function(err, result) {
           console.log('INSERTED INTO : ' + collectionHolder[i]);
-        // Broadcast the message to every client 
-        wss.clients.forEach(function (client) {
-          client.send(JSON.stringify(result));
+          // Broadcast the message to every client 
+          wss.clients.forEach(function (client) {
+            client.send(JSON.stringify(result));
           });
         });
       });
@@ -189,49 +115,6 @@ for (let i = 0; i < oplogHolder.length; ++i) {
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
-
-// Setup the listener for any updates to the database
-/*oplog.on('update', function (doc) {
-  console.log(JSON.stringify(doc));
-  database.getParkingDataWebSockets(collection, function(err, result) {
-    // Broadcast the message to every client 
-    wss.clients.forEach(function (client) {
-      client.send(JSON.stringify(result));
-    });
-  });
-});
-
-oplog.on('insert', function (doc) {
-  console.log("INSERTED");
-  database.getParkingDataWebSockets(collection, function(err, result) {
-    // Broadcast the message to every client 
-    wss.clients.forEach(function (client) {
-      client.send(JSON.stringify(result));
-    });
-  });
-});
-
-oplog.on('delete', function (doc) {
-  console.log("DELETED");
-  database.getParkingDataWebSockets(collection, function(err, result) {
-    // Broadcast the message to every client 
-    wss.clients.forEach(function (client) {
-      client.send(JSON.stringify(result));
-    });
-  });
-});
-
-oplog.on('delete', function (doc) {
-  database.getParkingDataWebSockets(collection, function(err, result) {
-    // Broadcast the message to every client 
-    wss.clients.forEach(function (client) {
-      client.send(JSON.stringify(result));
-    });
-  });
-});*/
 
 // init websocket
 app.socketio = require('socket.io')(server, {
